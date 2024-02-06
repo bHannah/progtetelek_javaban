f79545800df1e2ed09a57ac90b7d7dec763f495d
= <=>
2024-02-06 09:28:23 +0100

    összegzés tétel java nyelven

---
+++ src/tombnelkul/ElemiProgTetelek.java
@@ -0,0 +1,32 @@
+package tombnelkul;
+
+public class ElemiProgTetelek {
+
+    public static void main(String[] args) {
+    
+    /* Python kód*/    
+    
+    /*def tetel():
+    n = int(input("N="))
+    while n < 0:
+        n = int(input("N="))
+    ossz = 0
+    for i in range(n+1):
+        ossz += i
+    print(f"Az első {n} db természetes szám összege: {ossz}")*/
+    
+    /*Java kód*/
+    
+    int n = 9;
+    while (n < 1){
+    /*n = int(input("N= "))*/
+    }
+    int ossz = 0;
+    for (int i = 0; i < n+1; i++) {
+        ossz += i;
+    }
+    System.out.println(ossz);
+    
+    }
+    
+}
